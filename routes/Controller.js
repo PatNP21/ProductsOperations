df9396fe2583fff19f7b2a8addb5d0ee1f8157cc
PatNP21 <NaydoxP21@protonmail.com>
2023-11-22 16:56:58 +0100

    22112023

--- routes/Controller.js
+++ routes/Controller.js
@@ -9,7 +9,12 @@ class Controller {
         this.addProduct = this.addProduct.bind(this)
         this.updateProduct = this.updateProduct.bind(this)
         this.deleteProduct = this.deleteProduct.bind(this)
-        //pięc powyższych wierszy ... 
+        this.getOrders = this.getOrders.bind(this)
+        this.getOrderById = this.getOrderById.bind(this)
+        this.addOrder = this.addOrder.bind(this)
+        this.updateOrder = this.updateOrder.bind(this)
+        this.deleteOrder = this.deleteOrder.bind(this)
+        //dziesięc powyższych wierszy ... 
     }
     
     //pobieranie wszystkich produktów, czyli zwracanie rezultatów w zależności od powodzenia uzyskania wyników lub ich braku
@@ -50,6 +55,47 @@ class Controller {
 
     deleteProduct(req, res) {
         this.repository.deleteProduct(req.params.id).then(result => {
+            res.status(200).json(result)
+        }).catch(err => {
+            res.status(500).json(err.message)
+        })
+    }
+
+    getOrders(req, res) {
+        this.repository.getAllOrders().then(result => {
+            res.status(200).json(result)
+        }).catch(err => {
+            //obsługa wyjątku (tu błędnego wywołania metody)
+            res.status(500).json(err.message)
+        })
+    }
+
+    getOrderById(req, res) {
+        this.repository.getOrderById(req.params.id).then(result => {
+            res.status(200).json(result)
+        }).catch(err => {
+            res.status(500).json(err.message)
+        })
+    }
+
+    addOrder(req, res) {
+        this.repository.createOrder(req.body).then(() => {
+            res.status(201).json({"addedRecord": req.body})
+        }).catch(err => {
+            res.status(500).json(err.message)
+        })
+    }
+
+    updateOrder(req, res) {
+        this.repository.updateOrder(req.params.id, req.body).then(() => {
+            res.status(200).json({"updatedRecord": {"_id": req.params.id}})
+        }).catch(err => {
+            res.status(500).json(err.message)
+        })
+    }
+
+    deleteOrder(req, res) {
+        this.repository.deleteOrder(req.params.id).then(result => {
             res.status(200).json(result)
         }).catch(err => {
             res.status(500).json(err.message)
